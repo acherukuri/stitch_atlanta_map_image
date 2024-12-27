8e1d5ffc44001f7c96c80a449caef59e6d0e06fa
Anil Cherukuri <anil.cherukuri03@gmail.com>
2024-12-27 05:05:05 -0600

    Adds index page for downloading maps

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, render_template
 from generate_atlanta_map import StitchedAtlantaMapGenerator
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/download_atlanta_map', methods=['GET'])
 def download_atlanta_map_image():
     try:
         map_manifest_url = request.args.get('map_manifest_url')
