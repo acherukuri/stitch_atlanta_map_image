c1a2244b14796f6109128795327c45ce34757ee5
Anil Cherukuri <anil.cherukuri03@gmail.com>
2024-12-27 06:41:58 -0600

    Downloads images using javascript

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ def download_atlanta_map_image():
             return "Error: 'map_manifest_url' parameter is required", 400
 
         image_file_name = StitchedAtlantaMapGenerator(map_manifest_url).generate()
-        return send_file(f"./{image_file_name}", as_attachment=True, download_name='atlanta_map.jpg')
+        file = send_file(f"./{image_file_name}", as_attachment=True, download_name='atlanta_map.jpg')
+        os.remove(image_file_name)
+        return file
     except Exception as e:
         println(e)
         return "An error occurred while downloading the image. Please try again later.", 500
-    finally:
-        os.remove(image_file_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
