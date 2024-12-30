40e4ec9a000856b8df13168d3a6c8f111afefd00
Anil Cherukuri <anil.cherukuri03@gmail.com>
2024-12-30 11:07:32 -0600

    Enables logging with gunicorn

--- app.py
+++ app.py
@@ -25,3 +25,8 @@ def download_atlanta_map_image():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
